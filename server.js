6863c9dad0999fa8c2ac587608dc1d9ef486ea86
JScott <jvscott@gmail.com>
2014-06-21 14:46:45 -0400

    Simple db/server
    Set up paths for the page
    Added a path for new pages
    Allowed viewing existing pages

--- server.js
+++ server.js
@@ -1,8 +1,58 @@
 var express = require('express');
 var app = express();
+//app.enable('trust proxy');
+
+var http = require('http');
+var server = http.createServer(app);
 
 var cradle = require('cradle');
 var db = new(cradle.Connection)().database('mvml');
+db.exists(function (err, exists) {
+	if (err) {
+		console.log('error', err);
+	} else if (!exists) {
+		db.create();
+	}
+});
+
+var short_id = require('shortid');
+
+app.get('/', function(request, response) {
+	response.send('main page');
+});
+
+app.get('/new', function(request, response) {
+	var new_id = short_id.generate();
+	db.save(new_id, {
+		mvml: '', html: ''
+	}, function(error, db_response) {
+		//var rev = db_response.rev;
+		response.redirect('/edit/'+new_id);
+	});
+});
+
+app.get('/:id', function(request, response) {
+	db.get(request.params.id, function(error, document) {
+		response.send(document);
+	});
+});
+
+app.get('/edit/:id', function(request, response) {
+	response.send('edit '+request.params.id);
+});
+
+app.get('/account', function(request, response) {
+	response.send('your account');
+});
+
+app.get('/account/:id', function(request, response) {
+	response.send(request.params.id+'\'s account');
+});
+
+server.listen(6865);
+console.log('mvml-host server started on port '+server.address().port);
+
+
 
 /*
 db.get('vader', function (err, doc) {
