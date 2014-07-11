b07b8a54da068b566fb5d27faf4ee815d7b487df
JScott <jvscott@gmail.com>
2014-07-11 19:40:33 -0400

    Revert "WORK IN PROGRESS"
    This reverts commit 2dd4092b940c19464bc54902b93347c3bd7a6865.

--- public/js/FlyControls.js
+++ public/js/FlyControls.js
@@ -2,14 +2,16 @@
  * @author James Baicoianu / http://www.baicoianu.com/
  */
 
-THREE.FlyControls = function ( camera, mesh ) {
+THREE.FlyControls = function ( camera, mesh, collision_commander ) {
 
   this.camera = camera;
   this.mesh = mesh;
+  this.collision = collision_commander;
 
   this.domElement = document;
 
   // API
+  this.gravity = 9.8;
   this.movementSpeed = 1.0;
   this.rollSpeed = 0.005;
 
@@ -202,6 +204,12 @@ THREE.FlyControls = function ( camera, mesh ) {
     var movement = right.clone().multiplyScalar(this.moveVector.x);
     movement.add( forward.multiplyScalar(-this.moveVector.z) );
     movement.normalize().multiplyScalar(moveMult);
+    movement.add( up.multiplyScalar(-this.gravity) );
+
+    var distance = this.collision.distance( this.mesh, movement );
+    if (distance > 0) {
+      movement.normalize().multiplyScalar(distance - collision.theta);
+    }
 
     //if (clock.elapsedTime % 1 < 0.01) console.log(movement);
     this.camera.position.add(movement);
