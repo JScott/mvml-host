b1edb0ae236ebc6a16bc476814aaa35df8111d4e
JScott <jvscott@gmail.com>
2014-07-16 20:02:49 -0400

    Fixed a bug-to-be but not the main issue

--- public/js/CollisionCommander.js
+++ public/js/CollisionCommander.js
@@ -1,7 +1,7 @@
 THREE.CollisionCommander = function () {
 
   this.mesh_list = [];
-  this.theta = 0.01;
+  this.theta = 0.1;
 
   this.distance = function(mesh, movement) {
     // TODO: better vertices. slow and some vertices seem to take priority or something? swapping these *shouldn't* make a difference
@@ -14,7 +14,10 @@ THREE.CollisionCommander = function () {
       var ray = new THREE.Raycaster(global_vertex, movement.clone());
       var results = ray.intersectObjects(this.mesh_list);
       if (results.length > 0 && results[0].distance < movement.length()) {
-        return results[0].distance;
+        var shortest_distance = results.reduce(function(previous, result) {
+          return Math.min(previous, result.distance);
+        }, Number.POSITIVE_INFINITY);
+        return shortest_distance;
       }
     }
     return -1;
